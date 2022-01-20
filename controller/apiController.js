421588de40884553b39c31aacf6511b0467ea661
ayeAlvo <ayealvo@gmail.com>
2022-01-19 22:08:33 -0300

    agrego localidades al home y un poco de bootstrap

--- controller/apiController.js
+++ controller/apiController.js
@@ -5,13 +5,17 @@ module.exports = {
     fetch('https://ws.smn.gob.ar/map_items/weather')
     .then( response => response.json())
     .then( datas => {
-      let arrayProvinces = datas.map( valor => valor.province);
-      let setArrayProvinces = new Set(arrayProvinces); // crea un Conjunto No hay elementos repetidos en un array
-      // console.log(data);
+      const arrayProvinces = datas.map( valor => valor.province);
+      const setArrayProvinces = new Set(arrayProvinces); // crea un Conjunto No hay elementos repetidos en un array
+      const laPlata = datas.find(data => data.name == 'La Plata');
+      const cordoba = datas.find(data => data.name == 'Córdoba');
+      // console.log(cordoba);
       // console.log(setArrayDeProvincias);
       res.render('home', { 
         data: datas,
         provinces: setArrayProvinces,
+        laPlata,
+        cordoba,
       })
     })
   },
@@ -25,8 +29,9 @@ module.exports = {
        const provincieChoose = datas.filter(data => data.province == province);
        return res.render('filter', {
          data: datas,
+         nameProv: province, 
          province: provincieChoose,
-         nameProv: province 
+         
        });
       }      
     })    
