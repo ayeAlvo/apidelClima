381669e632a792f3fcfb99fdc4af613e60491da8
ayeAlvo <ayealvo@gmail.com>
2022-01-20 02:04:40 -0300

    agrego iconos y partials a las vistas

--- controller/apiController.js
+++ controller/apiController.js
@@ -16,6 +16,7 @@ module.exports = {
       const rosario = datas.find(data => data.name == 'Rosario');
       const caba = datas.find(data => data.name == 'Capital Federal');
       provincesHome.push(laPlata, cordoba, sanLuis, mendoza, rosario, caba);
+      res.locals = {provinces: setArrayProvinces}
       // console.log(provincesHome);
       // console.log(setArrayDeProvincias);
       res.render('home', { 
@@ -30,6 +31,10 @@ module.exports = {
     fetch('https://ws.smn.gob.ar/map_items/weather')
     .then( response => response.json())
     .then( datas => {
+      const arrayProvinces = datas.map( valor => valor.province);
+      const setArrayProvinces = new Set(arrayProvinces); // crea un Conjunto No hay elementos repetidos en un array
+      res.locals = {provinces: setArrayProvinces}
+
       let province = req.body.valor;
       if(province){
        const provincieChoose = datas.filter(data => data.province == province);
